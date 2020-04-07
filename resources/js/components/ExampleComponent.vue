abcce88396d331b795f2e77fae8319ce9e540014
Kelechi Okwuriki <kelechiokwuriki@Kelechis-MBP.broadband>
2020-04-07 20:15:48 +0100

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+           <div class="col-sm-4">
+               <div class="card">
+
+               </div>
+           </div>
+           <div class="col-sm-8">
+               tables
+           </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                stats: [],
+                apiUrl: 'https://api.covid19api.com/'
+            }
+        },
+        methods: {
+            getStats() {
+                axios.get(this.apiUrl)
+                .then(response => {
+                    this.stats = response.data;
+                })
+            }
+        },
+
+        mounted() {
+            this.getStats();
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">CoronaVirusStats</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Summary <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Countries</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+
+    </body>
+</html>
