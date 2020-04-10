e654a21862d13e534ad50d69fb56907d16ba463d
Kelechi Okwuriki <kelechiokwuriki@Kelechis-MBP.broadband>
2020-04-10 22:56:10 +0100

    match any route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                    <a class="nav-link" href="/">Data <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">Dashboard <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/information">Information <span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
                 <span class="navbar-text">
