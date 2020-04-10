711972c1299863755ba4bfc9124cc4d395100663
Kelechi Okwuriki <kelechiokwuriki@Kelechis-MBP.broadband>
2020-04-10 01:09:08 +0100

    http custom css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
         if(config('app.env') === 'production') {
             \URL::forceScheme('https');
+        }else{
+            \URL::forceScheme('http');
         }
     }
 }
