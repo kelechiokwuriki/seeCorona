93ed5848b758841f28a4b038030ad13411ea4d69
Kelechi Okwuriki <kelechiokwuriki@Kelechis-MBP.broadband>
2020-04-13 09:54:27 +0100

    country statistics refactored

--- database/seeds/CountryStatisticsSeeder.php
+++ database/seeds/CountryStatisticsSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 
 use App\CountryStatistics;
+use Carbon\Carbon;
+
 
 
 class CountryStatisticsSeeder extends Seeder
@@ -14,6 +16,23 @@ class CountryStatisticsSeeder extends Seeder
      */
     public function run()
     {
-        factory(CountryStatistics::class)->create();
+        $apiUrl = config('app.api_url');
+
+        $summaryResponse = Http::get($apiUrl.'/summary');
+
+        foreach ($summaryResponse['Countries'] as $value) {
+
+            CountryStatistics::create([
+                'country' => $value['Country'],
+                'country_new_confimed' => $value['NewConfirmed'],
+                'country_total_confimed' => $value['TotalConfirmed'],
+                'country_new_deaths' => $value['NewDeaths'],
+                'country_total_deaths' => $value['TotalDeaths'],
+                'country_new_recovered' => $value['NewRecovered'],
+                'country_total_recovered' => $value['TotalRecovered'],
+                'date_updated' => Carbon::parse($value['Date'])->toDateTimeString()
+            ]);
+        }
     }
+
 }
